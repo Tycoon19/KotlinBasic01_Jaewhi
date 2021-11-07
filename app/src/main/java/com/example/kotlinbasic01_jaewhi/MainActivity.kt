ea6d67b87569637ab3b34f7d0e95f602d032e1fa
Tycoon19 <kevin960831@naver.com>
2021-11-07 17:31:55 +0900

    로그 찍어보기

--- app/src/main/java/com/example/kotlinbasic01_jaewhi/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic01_jaewhi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinbasic01_jaewhi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+    clickBtn.setOnClickListener {
+        Log.d("메인화면","클릭용 버튼 눌림")
+    }
 
     }
 }
